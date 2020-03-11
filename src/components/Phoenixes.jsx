5e196734ed3e6af8ad768f6b245656f242aa6272
Jitgitbit <rowowolf@gmail.com>
2020-03-11 15:06:16 +0100

    conditional     TERNARY    operator

--- src/components/Phoenixes.jsx
+++ src/components/Phoenixes.jsx
@@ -7,34 +7,45 @@ const Phoenixes = ({phoenixes}) => {  // even more DESTRUCTURED, props by name !
     // const {phoenixes} = props;    // These two mean exactly the same, but this line is the DESTRUCTURED way !!
     // const phoenix = this.props.phoenix;
 
-    const phoenixesList = phoenixes.map(phoenix => {
-      if(phoenix.age > 40){
-        return (
-          <div className='phoenix' key={phoenix.id}>
-            <div>Name: {phoenix.name}</div>
-            <div>Age: {phoenix.age}</div>
-            <div>Belt: {phoenix.belt}</div>
-            <p>-------------------</p>
-          </div>
-        )
-      }else{
-        return null;
-      }
-      
-    })
+    // const phoenixesList = phoenixes.map(phoenix => {
+    //   if(phoenix.age > 40){
+    //     return (
+    //       <div className='phoenix' key={phoenix.id}>
+    //         <div>Name: {phoenix.name}</div>
+    //         <div>Age: {phoenix.age}</div>
+    //         <div>Belt: {phoenix.belt}</div>
+    //         <p>-------------------</p>
+    //       </div>
+    //     )
+    //   }else{
+    //     return null;
+    //   }  
+    // })
 
-
-    return (
-      // <div className='phoenix'>
-      //   <div>Name: {name}</div>
-      //   <div>Age: {age}</div>
-      //   <div>Belt: {belt}</div>
-      // </div>
-      <div className="phoenix-list">
-        {phoenixesList}
+  const phoenixesList = phoenixes.map(phoenix => {
+    // return condition ? () : ()
+    return phoenix.age > 40 ? (
+      <div className='phoenix' key={phoenix.id}>
+        <div>Name: {phoenix.name}</div>
+        <div>Age: {phoenix.age}</div>
+        <div>Belt: {phoenix.belt}</div>
+        <p>-------------------</p>
       </div>
-    )
-  }
+    ) : null;
+  });
+
+
+  return (
+    // <div className='phoenix'>
+    //   <div>Name: {name}</div>
+    //   <div>Age: {age}</div>
+    //   <div>Belt: {belt}</div>
+    // </div>
+    <div className="phoenix-list">
+      {phoenixesList}
+    </div>
+  )
+}
 
 
 export default Phoenixes;
